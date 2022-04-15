253b636da061e7f2d5aaa75ee9f2e2a2bdc77528
LedApalachee <ledapalachee@gmail.com>
2022-04-15 09:55:23 +0700

    added effects.h

---
+++ src/effects.h
@@ -0,0 +1,35 @@
+#ifndef effects_h
+#define effects_h
+
+#include <stdint.h>
+
+
+typedef struct Effect
+{
+	uint16_t type;
+	int time_left;
+} Effect;
+
+
+typedef enum EffectType
+{
+	EFFECT_NONE,
+
+	// physiological
+	EFFECT_BURNING,
+	EFFECT_BLEEDING,
+	EFFECT_FAINT,
+	EFFECT_BLIND,
+
+	// behavioral-psychological
+	EFFECT_CONFUSED,
+	EFFECT_TERRIFIED,
+	EFFECT_ENCOURAGED,
+	EFFECT_LOSTMINDED
+
+	// there will be more effects
+
+} EffectType;
+
+
+#endif
